18ee51bf3c69759a49743f1a95d6ddff846a9383
Scott Gonyea <sgonyea@truecar.com>
2011-11-25 01:05:28 -0800

    Initial Commit

---
+++ CookieInspector/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  CookieInspector
+#
+#  Created by Scott Gonyea on 11/25/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ CookieInspector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CookieInspector
+//
+//  Created by Scott Gonyea on 11/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
