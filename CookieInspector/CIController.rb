fcebf2a5c36fb51ccc73ed744e88cd8c003d5b01
Scott Gonyea <sgonyea@truecar.com>
2011-11-28 12:48:33 -0800

    Created a CIController, to interface the view elements with the CookieInspector helper.

---
+++ CookieInspector/CIController.rb
@@ -0,0 +1,13 @@
+#
+#  CIController.rb
+#  CookieInspector
+#
+#  Created by Scott Gonyea on 11/28/11.
+#  Copyright 2011 sgonyea inc. All rights reserved.
+#
+class CIController
+  attr_writer :cookiesTable
+
+  def reload(sender)
+  end
+end
