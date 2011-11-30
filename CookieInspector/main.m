f163aca2edc690957d231f6dde9eee018e1f35af
Scott Gonyea <sgonyea@truecar.com>
2011-11-29 18:14:52 -0800

    Indentation, yadda.

--- CookieInspector/main.m
+++ CookieInspector/main.m
@@ -2,13 +2,15 @@
 //  main.m
 //  CookieInspector
 //
-//  Created by Scott Gonyea on 11/25/11.
-//  Copyright 2011 sgonyea inc. All rights reserved.
+//  Created by Scott Gonyea on 11/28/11.
+//  Copyright (c) 2011 sgonyea inc. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+
 #import <MacRuby/MacRuby.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   return macruby_main("rb_main.rb", argc, argv);
 }
