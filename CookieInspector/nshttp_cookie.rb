26724a49aeecc25a4234f9199ee37d6c006da5c1
Scott Gonyea <sgonyea@truecar.com>
2011-11-29 18:15:44 -0800

    Added the #to_hash method. Added #expires_at, which calls #to_s on expiresDate.

--- CookieInspector/nshttp_cookie.rb
+++ CookieInspector/nshttp_cookie.rb
@@ -6,13 +6,28 @@
 #  Copyright 2011 sgonyea inc. All rights reserved.
 #
 class NSHTTPCookie
+  def to_hash
+    hash = {
+      'cookie'      => self,
+      'domain'      => domain,
+      'name'        => name,
+      'path'        => path,
+      'expires_at'  => expires_at,
+      'value'       => value
+    }
+  end
+
   def to_table
     table = {
       name => {
-        :path         => path,
-        :expiresDate  => expiresDate,
-        :value        => value
+        'path'        => path,
+        'expires_at'  => expires_at,
+        'value'       => value
       }
     }
+  end
+
+  def expires_at
+    expiresDate.to_s
   end
 end
